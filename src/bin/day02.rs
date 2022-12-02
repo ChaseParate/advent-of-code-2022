9ce6382f37cb26576ed1e709adf8afdc42d3c22d
ChaseParate <97500441+ChaseParate@users.noreply.github.com>
2022-12-01 23:07:25 -0700

    Replace for loops with iterators

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -4,58 +4,66 @@ use advent_of_code_2022::Puzzle;
 
 const PUZZLE: Puzzle = Puzzle::new(2);
 
-fn part_one(data: &Vec<(char, char)>) -> Result<u32, Box<dyn Error>> {
-    let mut score = 0;
-    for (opponent_move, my_move) in data {
-        score += match my_move {
-            'X' => 1,
-            'Y' => 2,
-            'Z' => 3,
-            _ => todo!(),
-        };
+fn part_one(rounds: &Vec<(char, char)>) -> Result<u32, Box<dyn Error>> {
+    let score = rounds
+        .iter()
+        .map(|(opponent_move, my_move)| {
+            let mut round_score = match (opponent_move, my_move) {
+                ('A', 'Z') | ('B', 'X') | ('C', 'Y') => 0,
+                ('A', 'X') | ('B', 'Y') | ('C', 'Z') => 3,
+                ('A', 'Y') | ('B', 'Z') | ('C', 'X') => 6,
+                _ => todo!(),
+            };
 
-        score += match (opponent_move, my_move) {
-            ('A', 'Z') | ('B', 'X') | ('C', 'Y') => 0,
-            ('A', 'X') | ('B', 'Y') | ('C', 'Z') => 3,
-            ('A', 'Y') | ('B', 'Z') | ('C', 'X') => 6,
-            _ => todo!(),
-        };
-    }
+            round_score += match my_move {
+                'X' => 1,
+                'Y' => 2,
+                'Z' => 3,
+                _ => todo!(),
+            };
+
+            round_score
+        })
+        .sum();
 
     Ok(score)
 }
 
-fn part_two(data: &Vec<(char, char)>) -> Result<u32, Box<dyn Error>> {
-    let mut score = 0;
-    for (opponent_move, result) in data {
-        let my_move = match (opponent_move, result) {
-            ('A', 'Y') | ('B', 'X') | ('C', 'Z') => 'X',
-            ('A', 'Z') | ('B', 'Y') | ('C', 'X') => 'Y',
-            ('A', 'X') | ('B', 'Z') | ('C', 'Y') => 'Z',
-            _ => todo!(),
-        };
+fn part_two(rounds: &Vec<(char, char)>) -> Result<u32, Box<dyn Error>> {
+    let score = rounds
+        .iter()
+        .map(|(opponent_move, result)| {
+            let my_move = match (opponent_move, result) {
+                ('A', 'Y') | ('B', 'X') | ('C', 'Z') => 'X',
+                ('A', 'Z') | ('B', 'Y') | ('C', 'X') => 'Y',
+                ('A', 'X') | ('B', 'Z') | ('C', 'Y') => 'Z',
+                _ => todo!(),
+            };
 
-        score += match my_move {
-            'X' => 1,
-            'Y' => 2,
-            'Z' => 3,
-            _ => todo!(),
-        };
+            let mut round_score = match my_move {
+                'X' => 1,
+                'Y' => 2,
+                'Z' => 3,
+                _ => todo!(),
+            };
 
-        score += match (opponent_move, my_move) {
-            ('A', 'Z') | ('B', 'X') | ('C', 'Y') => 0,
-            ('A', 'X') | ('B', 'Y') | ('C', 'Z') => 3,
-            ('A', 'Y') | ('B', 'Z') | ('C', 'X') => 6,
-            _ => todo!(),
-        };
-    }
+            round_score += match (opponent_move, my_move) {
+                ('A', 'Z') | ('B', 'X') | ('C', 'Y') => 0,
+                ('A', 'X') | ('B', 'Y') | ('C', 'Z') => 3,
+                ('A', 'Y') | ('B', 'Z') | ('C', 'X') => 6,
+                _ => todo!(),
+            };
+
+            round_score
+        })
+        .sum();
 
     Ok(score)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let puzzle_input = PUZZLE.get_input()?;
-    let data: Vec<(char, char)> = puzzle_input
+    let rounds: Vec<(char, char)> = puzzle_input
         .lines()
         .map(|line| {
             let (x, y) = line.split_once(' ').unwrap();
@@ -63,8 +71,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         })
         .collect();
 
-    println!("Part 1: {}", part_one(&data)?);
-    println!("Part 2: {}", part_two(&data)?);
+    println!("Part 1: {}", part_one(&rounds)?);
+    println!("Part 2: {}", part_two(&rounds)?);
 
     Ok(())
 }
